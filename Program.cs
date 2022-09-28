9332d4a0728006ab3499bf0713e2aa1ad9e55d75
Alex <tanko77@rambler.ru>
2022-09-28 21:29:05 +0300

    4 zadacha

--- Program.cs
+++ Program.cs
@@ -199,3 +199,73 @@
 // }
 
 
+// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+
+// Console.Clear();
+// int x = 2;
+// int y = 2;
+// int z = 2;
+
+// int[,,] array3number = new int[x, y, z];
+// FillArrayRandomNumbers(array3number);
+// WriteArray(array3number);
+
+// void WriteArray (int[,,] array3number)
+// {
+//   for (int i = 0; i < array3number.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array3number.GetLength(1); j++)
+//     {
+//         for (int k = 0; k < array3number.GetLength(2); k++)
+//       {
+//         Console.Write( $"{array3number[i,j,k]}({i},{j},{k})  ");
+//       }
+//       Console.WriteLine();
+//     }
+//   }
+// }
+
+// void FillArrayRandomNumbers(int[,,] array3number)
+// {
+//   int[] temp = new int[array3number.GetLength(0) * array3number.GetLength(1) * array3number.GetLength(2)];
+//   int  number;
+//   for (int i = 0; i < temp.GetLength(0); i++)
+//   {
+//     temp[i] = new Random().Next(10, 99);
+//     number = temp[i];
+//     if (i >= 1)
+//     {
+//       for (int j = 0; j < i; j++)
+//       {
+//         while (temp[i] == temp[j])
+//         {
+//           temp[i] = new Random().Next(10, 99);
+//           j = 0;
+//           number = temp[i];
+//         }
+//           number = temp[i];
+//       }
+//     }
+//   }
+//   int count = 0; 
+//   for (int x = 0; x < array3number.GetLength(0); x++)
+//   {
+//     for (int y = 0; y < array3number.GetLength(1); y++)
+//     {
+//       for (int z = 0; z < array3number.GetLength(2); z++)
+//       {
+//         array3number[x, y, z] = temp[count];
+//         count++;
+//       }
+//     }
+//   }
+// }
