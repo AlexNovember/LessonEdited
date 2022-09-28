69a980dd4f2e4a138e8f0849a57e6597fb91f142
Alex <tanko77@rambler.ru>
2022-09-28 18:17:17 +0300

    3

--- Program.cs
+++ Program.cs
@@ -70,55 +70,136 @@
 // и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
 
-Console.Clear();
-Console.Write("Введите количество строк массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество столбцов массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+// Console.Write("Введите количество строк массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int m = Convert.ToInt32(Console.ReadLine());
 
-int[,] numbers = new int[n, m];
-int Sum = 0, Sum1 = 0, Sum2 = 0, Row = 0;
+// int[,] numbers = new int[n, m];
+// int Sum = 0, Sum1 = 0, Sum2 = 0, Row = 0;
 
-FillArrayRandomNumbers(numbers);
-PrintArray(numbers);
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
 
            
-            for(int i = 0; i < n; i++) //Проходим по циклу строк
-            {
-                Sum2 = 0;
-                for(int j = 0; j < m; j++) //Проходим по циклу столбцов
-                {   
-                    Sum2 += numbers[i, j];  //Сумма всех членов 1ой строки.
-                } 
-                if(Sum2 < Sum1)
-                {   
-                    Sum = Sum2; Row = i+1;    
-                }
-                Sum1 = Sum2;
-            }
+//             for(int i = 0; i < n; i++) //Проходим по циклу строк
+//             {
+//                 Sum2 = 0;
+//                 for(int j = 0; j < m; j++) //Проходим по циклу столбцов
+//                 {   
+//                     Sum2 += numbers[i, j];  //Сумма всех членов 1ой строки.
+//                 } 
+//                 if(Sum2 < Sum1)
+//                 {   
+//                     Sum = Sum2; Row = i+1;    
+//                 }
+//                 Sum1 = Sum2;
+//             }
         
-            Console.WriteLine($"Наименьшая сумма элементов в строке {Row} = {Sum}");
-            Console.WriteLine();
+//             Console.WriteLine($"Наименьшая сумма элементов в строке {Row} = {Sum}");
+//             Console.WriteLine();
  
-void FillArrayRandomNumbers(int[,] array)
+// void FillArrayRandomNumbers(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 9);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write("{0,3}", array[i, j]);
+//         }
+//             Console.WriteLine();
+//     }
+// }
+
+
+
+
+// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Clear();
+// Console.WriteLine("Введите размеры матриц и диапазон случайных значений:");
+// int m = InputNumbers("Введите число строк 1-й матрицы: ");
+// int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+// int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+// int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+int[,] firstMartrix = new int[2, 2];
+FillArrayRandomNumbers(firstMartrix);
+Console.Write("Первая матрица:");
+PrintArray(firstMartrix);
+
+int[,] secomdMartrix = new int[2, 2];
+FillArrayRandomNumbers(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+PrintArray(secomdMartrix);
+
+int[,] resultMatrix = new int[2,2];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+PrintArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 9);
-        }
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
     }
+  }
 }
 
-void PrintArray(int[,] array)
+// int InputNumbers(string input)
+// {
+//   Console.Write(input);
+//   int output = Convert.ToInt32(Console.ReadLine());
+//   return output;
+// }
+
+void FillArrayRandomNumbers(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-            for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write("{0,3}", array[i, j]);
-        }
-            Console.WriteLine();
+      array[i, j] = new Random().Next(9);
     }
+  }
+}
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
 }
